de9c6b0bea51706b59d71a1650a6e93aa7174543
NazarMakarchukRB61 <makarchuk.nazar_rb61@edu.kpi.ua>
2026-09-24 14:38:05 +0300

    Add variant handling to calculate_y function
    Правки у коді (неправильно рахувався x) 
    Додання 9 варіанту

--- main.c
+++ main.c
@@ -3,24 +3,44 @@
 #include <math.h>
 
 //-----Функція для розрахунку y
-double calculate_y(double x) {
-    double y;
-
-    if (x >= -0.5 && x < 1.5) {
-        y = pow(x - 1.0, 3);
+    if (variant == 8) {
+        //----Варіант 8
+        if (x >= -100 && x < -50) {
+            y = -x;
+        }
+        else if (x >= 50 && x < 100) {
+            y = x;
+        }
+        else if (x >= 200 && x < 1000) {
+            y = log10(x) + 1;
+        }
+        else {
+            y = 0;
+        }
     }
-    else if (x >= 1.5 && x < 5.125) {
-        y = (x) - 2.0;
-    }
-    else if (x >= 5.125 && x < 9.5) {
-        y = 1.0 / (x + 10.0);
+    else if (variant == 9) {
+        //----Варіант 9
+        if (x >= 0.5 && x < 1.5) {
+            y = 2 / x - 1;
+        }
+        else if (x >= 1.5 && x < 5.125) {
+            y = x - 2;
+        }
+        else if (x >= 5.125 && x < 9.5) {
+            y = sqrt(x + 1) / sqrt(x * x - 10) - pow(x, 3);
+        }
+        else {
+            y = 1;
+        }
     }
     else {
-        y = pow(x, 2) - 1.0;
+        printf("Unknown variant!\n");
+        y = 0.0;
     }
 
     return y;
 }
+
 //---Оголошення змінних
 int main() {
     double x, y;
@@ -31,11 +51,12 @@ int main() {
     }
 
     //-----Розрахунок y
-    y = calculate_y(x);
+    y = calculate_y(x, variant);
 
     //-----Виведення результату
     system("cls");
 
+    printf("Variant = %d\n", variant);
     printf("x = %lf\n", x);
     printf("y = %lf\n", y);
 
